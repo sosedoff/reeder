7a6cd285747a9ce6214c57b5583f79d26ee19905
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-16 18:19:46 -0500

    Add opml parser

---
+++ lib/opml_parser.rb
@@ -0,0 +1,19 @@
+require "nokogiri"
+
+class OpmlParser
+  def parse_feeds(contents)
+    doc = Nokogiri.XML(contents)
+
+    doc.xpath("//body//outline").inject([]) do |feeds, outline|
+      next feeds if missing_fields? outline.attributes
+
+      feeds << outline.attributes["xmlUrl"].value
+    end
+  end
+
+  private
+  
+  def missing_fields?(attributes)
+    attributes["xmlUrl"].nil? || attributes["title"].nil?
+  end
+end
