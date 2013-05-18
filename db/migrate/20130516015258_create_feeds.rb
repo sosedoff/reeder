955caff457e0f69cca30aba7e4f831352b35fe8a
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-17 19:45:23 -0500

    Tweak feeds migrationgs

--- db/migrate/20130516015258_create_feeds.rb
+++ db/migrate/20130516015258_create_feeds.rb
@@ -1,13 +1,18 @@
 class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
-      t.string :title
-      t.string :description
-      t.string :url
-      t.string :site_url
-      t.datetime :last_modified_at
-      t.string :status, limit: 8
+      t.string      :title
+      t.string      :description
+      t.string      :url
+      t.string      :site_url
+      t.datetime    :last_modified_at
+      t.string      :status, limit: 8
+      t.integer     :posts_count, default: 0
+      t.integer     :unread_posts_count, default: 0
       t.timestamps
     end
+
+    add_index :feeds, :url
+    add_index :feeds, :last_modified_at
   end
 end
