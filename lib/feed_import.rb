f567bd7244480d1d00ccf2ce0495bfda60932418
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-22 18:59:20 -0500

    Refactor feed import

--- lib/feed_import.rb
+++ lib/feed_import.rb
@@ -10,14 +10,7 @@ class FeedImport
       record = Feed.find_by_url(feed.feed_url) 
       return record if record.present?
 
-      Feed.create(
-        title:            feed.title,
-        description:      feed.description,
-        url:              feed.feed_url,
-        site_url:         feed.url,
-        last_modified_at: feed.last_modified,
-        status:           'ok'
-      )
+      Feed.create(new_feed_attributes(feed))
     end
   end
 
@@ -26,5 +19,29 @@ class FeedImport
   def get_feed_details
     result = Feedzirra::Feed.fetch_and_parse(@url)
     result.class.name =~ /Feedzirra/ ? result : nil
+  end
+
+  def detect_site_url(feed)
+    if feed.url
+      feed.url
+    else
+      entry = (feed.entries || []).first
+
+      if entry
+        uri = URI.parse(entry.url)
+        "#{uri.scheme}://#{uri.host}"
+      end
+    end
+  end
+
+  def new_feed_attributes(feed)
+    {
+      title:            feed.title,
+      description:      feed.description,
+      url:              feed.feed_url,
+      site_url:         detect_site_url(feed),
+      last_modified_at: feed.last_modified,
+      status:           'ok'
+    }
   end
 end
