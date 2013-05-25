5c7655150735ebfa89a82eb5a6cf20f097f9522e
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-25 12:26:55 -0500

    Fix typo in attribute def

--- app/presenters/feed_presenter.rb
+++ app/presenters/feed_presenter.rb
@@ -2,5 +2,5 @@ class FeedPresenter < Presenter
   attribute :id, :title, :description
   attribute :url, :site_url
   attribute :last_modified_at, :status
-  attributes :created_at, :updated_at
+  attribute :created_at, :updated_at
 end
