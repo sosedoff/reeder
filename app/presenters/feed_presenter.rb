fb9e8c723f8426027cbf50c4771676c97af58191
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-25 13:01:38 -0500

    Include posts count into feed presenter

--- app/presenters/feed_presenter.rb
+++ app/presenters/feed_presenter.rb
@@ -2,5 +2,6 @@ class FeedPresenter < Presenter
   attribute :id, :title, :description
   attribute :url, :site_url
   attribute :last_modified_at, :status
+  attribute :posts_count, :unread_posts_count
   attribute :created_at, :updated_at
 end
