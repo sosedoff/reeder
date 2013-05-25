79381e61bcdd999d0eaba0027f73ed2f773292be
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-24 20:26:04 -0500

    Format into separate lines

--- app/presenters/feed_presenter.rb
+++ app/presenters/feed_presenter.rb
@@ -1,11 +1,6 @@
 class FeedPresenter < Presenter
-  attribute :id,
-            :title,
-            :description,
-            :url,
-            :site_url,
-            :last_modified_at,
-            :status,
-            :created_at,
-            :updated_at
+  attribute :id, :title, :description
+  attribute :url, :site_url
+  attribute :last_modified_at, :status
+  attributes :created_at, :updated_at
 end
