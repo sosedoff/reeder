1979f5e29c79d7cfb2125e93b48b03c0b8a20801
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-07-02 20:28:13 -0500

    Add /stats endpoint

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -3,6 +3,14 @@ class Reeder::Application
     json_error("Invalid route", 404)
   end
 
+  get '/stats' do
+    json_response(
+      users: User.count,
+      feeds: Feed.count,
+      posts: Post.count
+    )
+  end
+
   get '/*' do
     redirect '/'
   end
